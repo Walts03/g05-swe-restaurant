96178fa236a7d8bb425809ef2c60523e4e4c4da8
eli_0903 <91484965+Walts03@users.noreply.github.com>
2024-05-31 00:36:09 +1000

    update
    update

--- g05-swe-restaurant/src/components/Nav.vue
+++ g05-swe-restaurant/src/components/Nav.vue
@@ -1,65 +1,65 @@
 <template>
-  <nav class="bg-white border-gray-200 dark:bg-gray-900">
-    <div
-      class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4"
-    >
-      <router-link
-        to="/"
-        class="flex items-center space-x-3 rtl:space-x-reverse"
+  <div>
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+      <div
+        class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4"
       >
-        <img
-          src="https://flowbite.com/docs/images/logo.svg"
-          class="h-8"
-          alt="Flowbite Logo"
-        />
-        <span
-          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-          >My Restaurant</span
+        <router-link
+          to="/"
+          class="flex items-center space-x-3 rtl:space-x-reverse"
         >
-      </router-link>
-      <div class="flex items-center space-x-6 rtl:space-x-reverse">
-        <a
-          href="#"
-          class="text-sm text-blue-600 dark:text-blue-500 hover:underline"
-          >Login</a
-        >
+          <!-- <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            class="h-8"
+            alt="Flowbite Logo"
+          /> -->
+          <span
+            class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+            >My Restaurant</span
+          >
+        </router-link>
       </div>
-      <router-link to="/analytics">Analytics</router-link>
-    </div>
-  </nav>
-  <nav class="bg-gray-50 dark:bg-gray-700">
-    <div class="max-w-screen-xl px-4 py-3 mx-auto">
-      <div class="flex items-center">
-        <ul
-          class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm"
-        >
-          <li>
-            <router-link
-              to="/"
-              class="text-gray-900 dark:text-white hover:underline"
-              aria-current="page"
-              >Home
-            </router-link>
-          </li>
+    </nav>
+    <nav class="bg-gray-50 dark:bg-gray-700">
+      <div class="max-w-screen-xl px-4 py-3 mx-auto">
+        <div class="flex items-center">
+          <ul
+            class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm"
+          >
+            <li>
+              <router-link
+                to="/"
+                class="text-gray-900 dark:text-white hover:underline"
+                aria-current="page"
+                >Home
+              </router-link>
+            </li>
 
-          <li>
-            <a href="#" class="text-gray-900 dark:text-white hover:underline"
-              >Company</a
-            >
-          </li>
-          <li>
-            <a href="#" class="text-gray-900 dark:text-white hover:underline"
-              >Team</a
-            >
-          </li>
-          <li>
-            <a href="#" class="text-gray-900 dark:text-white hover:underline"
-              >Features</a
-            >
-          </li>
-        </ul>
+            <li>
+              <router-link
+                class="text-sm text-blue-600 dark:text-blue-500 hover:underline"
+                to="/analytics"
+                >Analytics</router-link
+              >
+            </li>
+            <li>
+              <router-link
+                class="text-sm text-blue-600 dark:text-blue-500 hover:underline"
+                to="/menuitems"
+                >Orders</router-link
+              >
+            </li>
+            <li>
+              <router-link
+                class="text-sm text-blue-600 dark:text-blue-500 hover:underline"
+                to="/cart"
+                >Cart</router-link
+              >
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 <script setup></script>
--- g05-swe-restaurant/src/router/index.js
+++ g05-swe-restaurant/src/router/index.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 import Default from "../components/Default.vue";
 import Homepage from "../views/Homepage.vue";
 import Analytics from "../components/Analytics.vue"; // Import the new component
+import MenuItems from "../components/MenuItems.vue";
+import AddMenuItem from "../components/AddMenuItem.vue";
+import EditItem from "../components/EditItem.vue";
+import Cart from "../components/Cart.vue";
 
 const routes = [
   {
@@ -14,13 +18,32 @@ const routes = [
         component: Homepage,
       },
       {
-        path: "analytics",
+        path: "/analytics",
         name: "Analytics",
         component: Analytics,
       },
+      {
+        path: "/menuitems",
+        name: "MenuItems",
+        component: MenuItems,
+      },
+      {
+        path: "/addmenu",
+        name: "AddMenuItem",
+        component: AddMenuItem,
+      },
+      {
+        path: "/edititem",
+        name: "EditItem",
+        component: EditItem,
+      },
+      {
+        path: "/cart",
+        name: "Cart",
+        component: Cart,
+      },
     ],
   },
- 
 ];
 
 const router = createRouter({
