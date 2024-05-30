87dd642826748b86e9c707bced0021ecca565f51
newbee1905 <beenewminh@outlook.com>
2024-05-30 23:27:35 +1000

    feat: add login, signup api

--- g05-swe-restaurant/src/views/LogIn.vue
+++ g05-swe-restaurant/src/views/LogIn.vue
@@ -1,49 +1,120 @@
 <template>
-    <Nav />
-    <section class="bg-gray-900">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <h1 class="text-xl text-white mb-6 font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Sign in to your account
-                    </h1>
-            <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <form class="space-y-4 md:space-y-6" action="#">
-                        <div>
-                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
-                        </div>
-                        <div>
-                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                        </div>
-                        <div class="flex items-center justify-between">
-                            <div class="flex items-start">
-                                <div class="flex items-center h-5">
-                                    <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
-                                </div>
-                                <div class="ml-3 text-sm">
-                                    <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                                </div>
-                            </div>
-                            <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                        </div>
-                        <button type="submit" class="w-full text-dark bg-[#FFC933] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Don’t have an account yet? <a href="/signup" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-                        </p>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>
-    <Footer />
+	<section class="bg-gray-900">
+		<div
+			class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
+		>
+			<h1
+				class="text-xl text-white mb-6 font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+			>
+				Sign in to your account
+			</h1>
+			<div
+				class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+			>
+				<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+					<form class="space-y-4 md:space-y-6" @submit.prevent="handleLogin">
+						<div>
+							<label
+								for="email"
+								class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>Your email</label
+							>
+							<input
+								type="email"
+								name="email"
+								id="email"
+								v-model="email"
+								class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								placeholder="name@company.com"
+								required
+							/>
+						</div>
+						<div>
+							<label
+								for="password"
+								class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+								>Password</label
+							>
+							<input
+								type="password"
+								name="password"
+								id="password"
+								v-model="password"
+								placeholder="••••••••"
+								class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+								required
+							/>
+						</div>
+						<button
+							type="submit"
+							class="w-full text-dark bg-[#FFC933] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+						>
+							Sign in
+						</button>
+						<p class="text-sm font-light text-gray-500 dark:text-gray-400">
+							Don't have an account yet?
+							<a
+								href="/signup"
+								class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+								>Sign up</a
+							>
+						</p>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
-import Nav from "../components/Nav.vue"
-import Footer from "../components/Footer.vue";
-
 export default {
-	name:"LogInPage"
-}
+	name: "LogInPage",
+	data() {
+		return {
+			email: '',
+			password: '',
+		};
+	},
+	methods: {
+		async handleLogin() {
+			try {
+				const response = await fetch('http://127.0.0.1:8000/users/login', {	// Update this URL to match your actual backend URL
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					body: JSON.stringify({
+						email: this.email,
+						password: this.password
+					}),
+					credentials: 'include' // Include this to handle cookies
+				});
+				
+				const result = await response.json();
+				console.log(result)
+				if (response.ok) {
+					this.$notify({
+						type: "success",
+						title: "Success",
+						text: 'Login successful!',
+					})
+					this.$router.push({ name: 'Homepage' });
+				} else {
+					this.$notify({
+						type: "error",
+						title: "Error",
+						text: `Error: ${result.detail || 'Login failed'}`,
+					})
+				}
+			} catch (error) {
+				this.$notify({
+					type: "error",
+					title: "Error",
+					text: `Error: ${error.message}'}`,
+				})
+			}
+		}
+	}
+};
 </script>
+
